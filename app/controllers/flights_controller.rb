dd671655da5470474671ce9fd999b502c154ff23
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-19 12:54:23 +1300

    write code to find flights corresponding to search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,7 +4,7 @@ class FlightsController < ApplicationController
         @available_dates = Flight.pluck(:start_datetime).map(&:to_date).uniq
 
         # logic for retrieving flights based on the selected date
-        
+        @flights = query_flights
     end
 
 
@@ -13,6 +13,27 @@ class FlightsController < ApplicationController
     def flight_params
         params.require(:flight)
             .permit(:departure_airport_id, :arrival_airport_id, 
-                    :num_passangers, :start_datetime)  
+                    :num_passengers, :start_datetime)  
+    end
+
+    def query_flights
+        # Build a query based on the submitted form criteria
+        flights_query = Flight.all
+
+        if params[:departure_airport_id].present?
+            flights_query = flights_query.where(departure_airport_id: params[:departure_airport_id])
+        end
+
+        if params[:arrival_airport_id].present?
+            flights_query = flights_query.where(arrival_airport_id: params[:arrival_airport_id])
+        end
+
+        if params[:start_datetime].present?
+            @start_datetime = params[:start_datetime]
+            # Assuming start_datetime is a DateTime field, adjust if necessary
+            flights_query = flights_query.where(start_datetime: @start_datetime)
+        end
+
+        flights_query
     end
 end
