e25d960c8c9e82a25d227673ea83096f3adef70d
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-21 10:52:51 +1300

    set up bookings #new action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,18 @@
 class BookingsController < ApplicationController
     def new
       @booking = Booking.new
+      @flight = params[:flight]
+      @num_passangers = params[:num_passengers]
     end
 
     def create 
         
+    end
+
+    private
+
+    def booking_params
+      params.require(:booking).permit(:num_passengers, :flight)
+      
     end
 end
