d766ac31a8619edf7f769534fc9353c9d826372c
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-26 20:31:51 +1300

    set default available flights to none

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,6 +5,7 @@ class FlightsController < ApplicationController
         
         # logic for retrieving flights based on the selected date
         @flights = query_flights
+        
         @num_passengers = params[:num_passengers]
     end
 
@@ -19,20 +20,30 @@ class FlightsController < ApplicationController
 
     def query_flights
         # Build a query based on the submitted form criteria
-        flights_query = Flight.all
+        flights_query = []
 
         if params[:departure_airport_id].present?
-            flights_query = flights_query.where(departure_airport_id: params[:departure_airport_id])
+            if flights_query.any?
+                flights_query = flights_query.where(departure_airport_id: params[:departure_airport_id])
+            else
+                flights_query = Flight.all.where(departure_airport_id: params[:departure_airport_id])
+            end
         end
 
         if params[:arrival_airport_id].present?
-            flights_query = flights_query.where(arrival_airport_id: params[:arrival_airport_id])
+            if flights_query.any?
+                flights_query = flights_query.where(arrival_airport_id: params[:arrival_airport_id])
+            else
+                flights_query = Flight.all.where(arrival_airport_id: params[:arrival_airport_id])
+            end
         end
 
         if params[:start_datetime].present?
-            @start_datetime = params[:start_datetime]
-            # Assuming start_datetime is a DateTime field, adjust if necessary
-            flights_query = flights_query.where(start_datetime: @start_datetime)
+            if flights_query.any?
+                flights_query = flights_query.where(start_datetime: params[:start_datetime])
+            else
+                flights_query = Flight.all.where(start_datetime: params[:start_datetime])
+            end
         end
 
         flights_query
